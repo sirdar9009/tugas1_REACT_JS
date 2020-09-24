9451f18be7a44d4a520b0af7d668cfcd3c4ee866
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-24 08:22:33 +0700

    Add files via upload

---
+++ precache-manifest.b01ebac931b3235cce22ae6ab7fe33b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efad985e1caa00ca0db486ea9107f9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069a69b526860ebea02d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2c6e46505ad7b0ec537",
+    "url": "/static/js/2.2e2c8c34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e2c8c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069a69b526860ebea02d",
+    "url": "/static/js/main.213af1d7.chunk.js"
+  },
+  {
+    "revision": "23363dbf6347cae36a63",
+    "url": "/static/js/runtime-main.a13e9720.js"
+  }
+]);
